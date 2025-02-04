97f437d67962cac60ba48c821522907263b448ca
solaiman-siam <solaimansiam93@gmail.com>
2025-02-04 10:26:17 +0600

    added vercel.json

--- src/lib/staticData.js
+++ src/lib/staticData.js
@@ -1,27 +1,27 @@
-import CakeIcon from '../assets/images/Birthday.png'
-import Cart from '../assets/images/Cart.png'
-import Email from '../assets/images/Email.png'
+// import CakeIcon from '../assets/images/Birthday.png'
+// import Cart from '../assets/images/Cart.png'
+// import Email from '../assets/images/Email.png'
 
-export const chooseCardData = 
+// export const chooseCardData = 
     
-        [
-            {
-            "id": 1,
-              "title": "Customer’s Birthdays",
-              "description": "Celebrate with us! Enjoy exclusive discounts and personalized offers on your special day. Join our birthday club for VIP treatment and unforgettable surprises.",
-              "icon": CakeIcon
-            },
-            {
-                "id": 2,
-              "title": "Festive Greetings",
-              "description": "Renew your vehicle hassle-free with us! Enjoy streamlined processes, timely reminders, and expert assistance. Stay on the road smoothly with our efficient renewal services.",
-              "icon": Email
-            },
-            {
-                "id": 3,
-              "title": "Anniversaries of any kind",
-              "description": "Celebrate milestones with us! Whether it's a work anniversary, relationship milestone, or any special occasion, mark the moment with our tailored surprises and heartfelt gestures.",
-              "icon": Cart
-            }
-          ]
+//         [
+//             {
+//             "id": 1,
+//               "title": "Customer’s Birthdays",
+//               "description": "Celebrate with us! Enjoy exclusive discounts and personalized offers on your special day. Join our birthday club for VIP treatment and unforgettable surprises.",
+//               "icon": CakeIcon
+//             },
+//             {
+//                 "id": 2,
+//               "title": "Festive Greetings",
+//               "description": "Renew your vehicle hassle-free with us! Enjoy streamlined processes, timely reminders, and expert assistance. Stay on the road smoothly with our efficient renewal services.",
+//               "icon": Email
+//             },
+//             {
+//                 "id": 3,
+//               "title": "Anniversaries of any kind",
+//               "description": "Celebrate milestones with us! Whether it's a work anniversary, relationship milestone, or any special occasion, mark the moment with our tailored surprises and heartfelt gestures.",
+//               "icon": Cart
+//             }
+//           ]
           
